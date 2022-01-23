2bb8a923b1bf17da8ca295c1863ff7ad702ad267
dean.shaked@xtrade.com <dean.shaked@xtrade.com>
2022-01-23 19:52:15 +0200

    first init

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="min-h-screen">
+        <div className="gradient-bg-welcome">
+          <Navbar />
+          <Welcome />
+        </div>
+        <Services />
+        <Transactions />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+import logo from "../../../images/logo.png";
+
+import NavbarItem from "./NavbarItem";
+
+const titles = ["Markets","Exchange", "Tutorial", "Wallets"]
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.5] flex-initial justify-center items-center">
+        <img src={logo} alt="" className="w-32 cursor-pointer" />
+      </div>
+      <ul className="text-white md:flex hidden flex-row justify-between items-center flex-initial">
+        {titles.map(title => <NavbarItem title={title} classProps={} />)}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
