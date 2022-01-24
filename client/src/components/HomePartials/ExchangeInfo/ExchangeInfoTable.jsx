9e669a1c8a65faa48159e772959692f41f687bdf
dean.shaked@xtrade.com <dean.shaked@xtrade.com>
2022-01-25 00:17:47 +0200

    little changes

--- client/src/components/HomePartials/ExchangeInfo/ExchangeInfoTable.jsx
+++ client/src/components/HomePartials/ExchangeInfo/ExchangeInfoTable.jsx
@@ -1,21 +1,21 @@
 // Components
 import BNBLogo from "../../../../assets/images/bnb-logo.svg";
 import ETHLogo from "../../../../assets/images/eth-logo.svg";
+import { IoSwapVerticalSharp } from "react-icons/io5";
 
 const ExchangeInfoTable = () => {
   return (
-    <div className="flex flex-col bg-grey h-64 w-2/5 rounded-2xl">
-      <div className="flex flex-col">
-        <div className="flex flex-row bg-greyish h-32 justify-center align-center rounded-xl">
-          <img src={BNBLogo} className="h-10 w-10" />
-          <h1>{"BNB"}</h1>
-        </div>
+    <div className="flex flex-col bg-grey h-72 w-1/2 rounded-2xl relative">
+      <div className="flex flex-row bg-greyish h-1/2 justify-center items-center ">
+        <img src={BNBLogo} className="h-10 w-10" />
+        <h1 className="">{"BNB"}</h1>
+      </div>
+      <IoSwapVerticalSharp className=" cursor-pointer w-12 h-12 absolute top-[42%] right-[47%] rounded-xl bg-grey text-green" />
 
-        {/* <div className="">
+      {/* <div className="">
           <img src={ETHLogo} className="h-10 w-10" />
           <h1>{"ETH"}</h1>
         </div> */}
-      </div>
     </div>
   );
 };
