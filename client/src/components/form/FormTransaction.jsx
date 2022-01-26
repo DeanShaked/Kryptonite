3219f1a9143ea4957ad53dc3ce264aa286a72977
ido.Adar <idox2x@gmail.com>
2022-01-27 01:17:02 +0200

    ...

--- client/src/components/form/FormTransaction.jsx
+++ client/src/components/form/FormTransaction.jsx
@@ -1,32 +1,66 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-// Components
-import InputElement from '../Reusable/InputElement'
-import ButtonElement from '../Reusable/ButtonElement'
+import InputElement from '../Reusable/InputElement';
+import ButtonElement from '../Reusable/ButtonElement';
 
 const initState = {
-    addressTo: null,
-    amount: null,
-    keyword: null,
-    message: null
-},
+  addressTo: '',
+  amount: '',
+  keyword: '',
+  message: '',
+};
 
 const FormTransaction = () => {
-    // const [transactionFormState, setTransactionFormState] = useState(initState);
+  const [stateTransaction, setStateTransaction] = useState(initState);
 
-    // const { addressTo, amount, keyword, message } = transactionFormState;
+  const updateState = (name, event) => {
+    setStateTransaction((prevState) => ({
+        ...prevState,
+        [name]: event.target.value
+    }))
+  }
 
-    // const updateState = () => {}
+  const onSend = () => {
+      console.log(stateTransaction);
+      setStateTransaction(initState);
+  }
 
-    return (
-        <div>
-            {/* <InputElement value={addressTo} placeholder={'Address To'} onChange={updateState} name={'addressTo'} />
-            <InputElement value={amount} placeholder={'Amount'} onChange={updateState} name={'amount'} />
-            <InputElement value={keyword} placeholder={'Keyword'} onChange={updateState} name={'keyword'} />
-            <InputElement value={message} placeholder={'Message'} onChange={updateState} name={'message'} />
-            <ButtonElement title={'Send Now'} backgroundColor={'green'} textColor={'white'} onClick={() => {}} /> */}
-        </div>
-    )
-}
+  const { addressTo, amount, keyword, message } = stateTransaction;
+
+  return (
+    <div>
+      <InputElement
+        value={addressTo}
+        placeholder={'Address To'}
+        onChange={updateState.bind(this, 'addressTo')}
+        name={'addressTo'}
+      />
+      <InputElement
+        value={amount}
+        placeholder={'Amount'}
+        onChange={updateState.bind(this, 'amount')}
+        name={'amount'}
+      />
+      <InputElement
+        value={keyword}
+        placeholder={'Keyword'}
+        onChange={updateState.bind(this, 'keyword')}
+        name={'keyword'}
+      />
+      <InputElement
+        value={message}
+        placeholder={'Message'}
+        onChange={updateState.bind(this, 'message')}
+        name={'message'}
+      />
+      <ButtonElement
+        title={'Send Now'}
+        backgroundColor={'green'}
+        textColor={'white'}
+        onClick={onSend}
+      />
+    </div>
+  );
+};
 
 export default FormTransaction;
