0fbf1b09d117d51a5b97fa96fea9887e4fa75a4f
dean.shaked@xtrade.com <dean.shaked@xtrade.com>
2022-01-27 01:25:16 +0200

    last commit wen

--- client/src/components/form/FormTransaction.jsx
+++ client/src/components/form/FormTransaction.jsx
@@ -1,32 +1,56 @@
 import React, { useState } from "react";
 
 // Components
-import InputElement from '../Reusable/InputElement'
-import ButtonElement from '../Reusable/ButtonElement'
-
-const initState = {
-    addressTo: null,
-    amount: null,
-    keyword: null,
-    message: null
-},
+import InputElement from "../Reusable/InputElement";
+import ButtonElement from "../Reusable/ButtonElement";
 
 const FormTransaction = () => {
-    // const [transactionFormState, setTransactionFormState] = useState(initState);
+  const initState = {
+    addressTo: "",
+    amount: "",
+    keyword: "",
+    message: ""
+  },
+  const [transactionFormState, setTransactionFormState] = useState({});
 
-    // const { addressTo, amount, keyword, message } = transactionFormState;
+  const { addressTo, amount, keyword, message } = transactionFormState;
 
-    // const updateState = () => {}
+  const updateState = () => {};
 
-    return (
-        <div>
-            {/* <InputElement value={addressTo} placeholder={'Address To'} onChange={updateState} name={'addressTo'} />
-            <InputElement value={amount} placeholder={'Amount'} onChange={updateState} name={'amount'} />
-            <InputElement value={keyword} placeholder={'Keyword'} onChange={updateState} name={'keyword'} />
-            <InputElement value={message} placeholder={'Message'} onChange={updateState} name={'message'} />
-            <ButtonElement title={'Send Now'} backgroundColor={'green'} textColor={'white'} onClick={() => {}} /> */}
-        </div>
-    )
-}
+  return (
+    <div>
+      <InputElement
+        value={addressTo}
+        placeholder={"Address To"}
+        onChange={updateState}
+        name={"addressTo"}
+      />
+      <InputElement
+        value={amount}
+        placeholder={"Amount"}
+        onChange={updateState}
+        name={"amount"}
+      />
+      <InputElement
+        value={keyword}
+        placeholder={"Keyword"}
+        onChange={updateState}
+        name={"keyword"}
+      />
+      <InputElement
+        value={message}
+        placeholder={"Message"}
+        onChange={updateState}
+        name={"message"}
+      />
+      <ButtonElement
+        title={"Send Now"}
+        backgroundColor={"green"}
+        textColor={"white"}
+        onClick={() => {}}
+      />
+    </div>
+  );
+};
 
 export default FormTransaction;
