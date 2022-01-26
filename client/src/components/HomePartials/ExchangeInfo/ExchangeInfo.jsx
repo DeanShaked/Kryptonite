7a34cd9b087149ba96bce827a89e7b5731fae088
Dean <dean.shaked@xtrade.com>
2022-01-26 15:50:00 +0200

    added some

--- client/src/components/HomePartials/ExchangeInfo/ExchangeInfo.jsx
+++ client/src/components/HomePartials/ExchangeInfo/ExchangeInfo.jsx
@@ -4,11 +4,21 @@ import ExchangeInfoTable from "./ExchangeInfoTable";
 
 const ExchangeInfo = () => {
   return (
-    <div className="container mx-auto bg-green h-[700px] flex flex-col  xl:flex-row my-2">
-      <div className="flex flex-row  bg-white w-full h-full">
-        <h1>hey</h1>
-        <h1>hey</h1>
-        <h1>hey</h1>
+    <div className="container mx-auto bg-transparent h-[700px] flex flex-col  xl:flex-row my-2">
+      <div className="flex flex-col bg-black w-full h-full">
+        <h1 className="font-bold text-6xl text-white">
+          Trusted and secure bitcoin and crypto exchange{" "}
+        </h1>
+        <p className="font-medium text-[#CAC5CA]">
+          Get Started with the easiest and most secure platform to buy, sell,
+          trade, and earn cryptocurrencies
+        </p>
+        <ButtonElement
+          title="Get Started"
+          backgroundColor="green"
+          textColor="white"
+          onClick={{}}
+        />
       </div>
       <div className="flex flex-rowbg-black w-full h-full">
         <h1>hey</h1>
--- client/src/components/Reusable/ButtonElement.jsx
+++ client/src/components/Reusable/ButtonElement.jsx
@@ -1,13 +1,11 @@
 const ButtonElement = ({ title, backgroundColor, textColor, onClick }) => {
   return (
-    <div className="flex flex-row w-full px-16 hover:transition-opacity hover:duration-300 hover:opacity-70 justify-end">
-      <button
-        onClick={onClick}
-        className={`text-${textColor} bg-${backgroundColor} h-10 w-40 rounded-xl font-bold`}
-      >
-        {title}
-      </button>
-    </div>
+    <button
+      onClick={onClick}
+      className={`text-${textColor} bg-${backgroundColor} h-10 w-40 rounded-xl font-bold hover:transition-opacity hover:duration-300 hover:opacity-70`}
+    >
+      {title}
+    </button>
   );
 };
 
