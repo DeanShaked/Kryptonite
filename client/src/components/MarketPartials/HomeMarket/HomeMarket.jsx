7860d2db57560bde64202747f179465047ffa653
Dean Shaked <deanshak9@gmail.com>
2022-03-05 21:50:54 +0200

    added content to home market

--- client/src/components/MarketPartials/HomeMarket/HomeMarket.jsx
+++ client/src/components/MarketPartials/HomeMarket/HomeMarket.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import AssetCard from "../../Reusable/AssetCard";
 
 const HomeMarket = () => {
-  return <div className="text-white">HomeMarket</div>;
+  return (
+    <div className="h-full flex flex-col items-center justify-start mt-0 mb-10">
+      {/* TODO: map over the nft cards */}
+      <div className="flex flex-wrap space-x-12 mx-4 justify-center mb-10">
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+        <AssetCard />
+      </div>
+    </div>
+  );
 };
 
 export default HomeMarket;
