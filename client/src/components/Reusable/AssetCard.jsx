d3db3254d0e0609b9d958c1f35e071ce33efb5f0
Dean Shaked <deanshak9@gmail.com>
2022-03-07 22:55:53 +0200

    ..

--- client/src/components/Reusable/AssetCard.jsx
+++ client/src/components/Reusable/AssetCard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const AssetCard = () => {
-  return <div className=" bg-primary w-52 h-[450px] mt-10">AssetCard</div>;
+  return (
+    <div className=" bg-grey w-52 h-[450px] mt-10 rounded-lg flex flex-col">
+      <div className="flex flex-grow">Body</div>
+      <div className="bg-black text-white h-[70px] rounded-lg text-center">
+        Price -{" "}
+      </div>
+    </div>
+  );
 };
 
 export default AssetCard;
