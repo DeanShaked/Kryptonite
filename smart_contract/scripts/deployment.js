49d5e8de4aa5324587a55c1c5abb3647b348385c
Dean Shaked <deanshak9@gmail.com>
2022-05-30 12:22:21 +0300

    mod

--- smart_contract/scripts/deployment.js
+++ smart_contract/scripts/deployment.js
@@ -1,15 +1,12 @@
 /* ------------------------ Transactions Contract ------------------------ */
-export const TransactionsContractAddress =
-  "0xEae40ccbB427d2D31AAE1ADFC2C5cA1388f84d4e";
-export const TransactionsOwnerAddress =
-  "0xE4c7eA7991cb495252a3dEF52865183221a23A09";
+export const TransactionsContractAddress = "0xdB64BbA9667B533b49f88C4317fA8407D5b5Fda3"
+export const TransactionsOwnerAddress = "0x2632E9065e2D6f317BD9e6694553F2c1AF88e9E4"
 
 /* ------------------------ NFTMarket Contract ------------------------ */
-export const NFTMarketContractAddress =
-  "0xf0Cb2f672AFD30D6244332bA775f254316076F63";
-export const NFTMarketOwnerAddress =
-  "0xE4c7eA7991cb495252a3dEF52865183221a23A09";
+export const NFTMarketContractAddress = "0x2f0E2f6d2882118bFDc1072F23ED45028AB14C6d"
+export const NFTMarketOwnerAddress = "0x2632E9065e2D6f317BD9e6694553F2c1AF88e9E4"
 
 /* ------------------------ NFT Contract ------------------------ */
-export const NFTContractAddress = "0xF7e4f9360743242f626ae86a5597422e015135d7";
-export const NFTOwnerAddress = "0xE4c7eA7991cb495252a3dEF52865183221a23A09";
+export const NFTContractAddress = "0xF42100D2d9DFAF00dB37bE554aDf4c0B1c5ed4a3"
+export const NFTOwnerAddress = "0x2632E9065e2D6f317BD9e6694553F2c1AF88e9E4"
+
